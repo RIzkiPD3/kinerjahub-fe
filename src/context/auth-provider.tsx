0d78824dd44bd8270d2bf2710bff651bfead58f6
zakicerdas <zakitoriq1@gmail.com>
2026-02-16 22:48:02 +0700

    meanambahkan context dan costum hook

---
+++ src/context/auth-provider.tsx
@@ -0,0 +1,15 @@
+import { useState, type ReactNode } from "react";
+import { AuthContext } from "./auth-context";
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
